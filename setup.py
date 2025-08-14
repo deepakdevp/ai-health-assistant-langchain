3cb3ac16570cc1f7dac417f90b0d53da1d5602a0
deepak21 <deepakdevp@gmail.com>
2025-08-14 21:48:56 +0900

    Add environment variables, update requirements, and configure setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="ai-health-assistant-langchain",
+    version="0.1.0",
+    author="Deepak Dev Panwar",
+    author_email="deepakdevp@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
